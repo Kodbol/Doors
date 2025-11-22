6f84d70e5c69e7b0f4cdac180ed2405d6fe070be
kodbol <p.sasha1701@gmail.com>
2025-11-22 16:11:13 +0300

    Update LightChanger.lua

--- Nightmare Mode/LightChanger.lua
+++ Nightmare Mode/LightChanger.lua
@@ -1,15 +1,16 @@
 return {
 
-Elevation = function()
-   print("test")
-end       
-
-
-
-
-
-
-
-
-
+ChangeLight = function(color)
+   local CurrentRoom = game.Workspace.CurrentRooms[tostring(LatestRoom.Value)]
+	for _, light in CurrentRoom.Assets:GetDescendants() do
+            if light.ClassName == "PointLight" or light.ClassName == "SpotLight" or light.ClassName == "SurfaceLight" then
+				local LightAnim = TweenService:Create(light, TweenInfo.new(2), {Color = color})
+			 LightAnim:Play()
+			elseif light.Name == "Neon" then
+				local NeonAnim = TweenService:Create(light, TweenInfo.new(2), {Color = color})
+			  NeonAnim:Play()
+	   end 
+	end
+end    
+   
 }
